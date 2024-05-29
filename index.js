0e7e99ae6b3485e5eb4ea1dcc8e98e3b59257eb1
vasant J <86471065+devil2603@users.noreply.github.com>
2024-05-29 18:00:12 +0530

    added all files for count passenger

---
+++ index.js
@@ -0,0 +1,25 @@
+
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+function increment(){
+
+    count = count + 1
+    countEl.innerText = count
+}
+
+
+
+function save(){
+    var countStr = count + " "
+    saveEl.innerText += countStr 
+    countEl.innerText = 0
+    console.log(countStr)
+    count = 0
+  
+}
+
+function remove(){
+    saveEl.innerText =  final
+}
